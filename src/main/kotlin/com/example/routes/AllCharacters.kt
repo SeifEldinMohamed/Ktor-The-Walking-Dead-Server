9273d94149d58f1e4da3e3f5dd05328af547d381
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-12-24 10:00:15 +0200

    - complete login of getAllCharacters in AllCharacters.kt

--- src/main/kotlin/com/example/routes/AllCharacters.kt
+++ src/main/kotlin/com/example/routes/AllCharacters.kt
@@ -1,19 +1,28 @@
 package com.example.routes
 
 import com.example.models.ApiResponse
+import com.example.repository.CharacterRepository
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import org.koin.ktor.ext.inject
 import java.lang.IllegalArgumentException
 import java.lang.NumberFormatException
 
 fun Route.getAllCharacters() {
+    val characterRepository: CharacterRepository by inject()
     get("/characters") {
         try {
-            val page = call.request.queryParameters["page"]?.toInt() ?: 1
+            val page = call.request.queryParameters["page"]?.toInt() ?: 1 // page get from call
             require(page in 1..5)
-            call.respond(message = page)
+
+            val apiResponse = characterRepository.getAllCharacters(page = page)
+
+            call.respond(
+                message = apiResponse,
+                status = HttpStatusCode.OK
+            )
         } catch (e: NumberFormatException) { // when user pass any type except int
             call.respond(
                 message = ApiResponse(success = false, message = "Only Numbers Allowed."),
@@ -23,7 +32,7 @@ fun Route.getAllCharacters() {
         catch (e:IllegalArgumentException){
             call.respond(
                 message = ApiResponse(success = false, message = "Characters not found."),
-                status = HttpStatusCode.BadRequest
+                status = HttpStatusCode.NotFound
             )
         }
     }
