234264949c2e5d1bdee88e6e2e2290ab78168a0b
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-12-30 11:04:40 +0200

    Root End Point Added

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,15 +1,11 @@
 package com.example.plugins
 
+import com.example.routes.root
 import io.ktor.server.routing.*
 import io.ktor.server.application.*
-import io.ktor.server.response.*
-
 
 fun Application.configureRouting() {
-
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
+        root()
     }
 }
