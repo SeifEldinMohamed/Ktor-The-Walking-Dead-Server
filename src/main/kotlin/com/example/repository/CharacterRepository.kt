f292ea30e818ebe6c4abcb00f5751cb239086a07
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-12-30 10:59:38 +0200

    Create CharacterRepository Interface

---
+++ src/main/kotlin/com/example/repository/CharacterRepository.kt
@@ -0,0 +1,17 @@
+package com.example.repository
+
+import com.example.models.ApiResponse
+import com.example.models.Character
+
+interface CharacterRepository {
+    val characters: Map<Int, List<Character>>
+
+    val page1: List<Character>
+    val page2: List<Character>
+    val page3: List<Character>
+    val page4: List<Character>
+    val page5: List<Character>
+
+    suspend fun getAllCharacters(page: Int = 1): ApiResponse
+    suspend fun searchCharacters(name: String): ApiResponse
+}
